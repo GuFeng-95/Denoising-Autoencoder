7ce695ccfde1e079283f10348210fccfeaf66911
GuFeng-95 <155743094+GuFeng-95@users.noreply.github.com>
2024-03-24 17:29:08 +0800

    Update gen_cylinder_data.py

--- gen_cylinder_data.py
+++ gen_cylinder_data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 for g in [0.35,1,5,10]:
-    cylinder_data = np.load('./cylinder_data/cylinder_clean.npy')
+    cylinder_data = io.loadmat('./cylinder_data/CYLINDER_ALL.mat')['VORTALL'].T
     gauss = np.random.normal(0, g, cylinder_data.shape).astype('float')
     cylinder_data_noise = cylinder_data + gauss
     np.save('./cylinder_data/cylinder_noise_{}.npy'.format(g),cylinder_data_noise)
